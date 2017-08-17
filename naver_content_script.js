cf33a8b2b99b76c89c0ba8b667ec238c568ab38a
gswon <bluemirr5@gmail.com>
2017-08-17 09:44:57 +0900

    add naver_content_logic

--- naver_content_script.js
+++ naver_content_script.js
@@ -3,18 +3,47 @@ var resultIndex = 0;
 var resultPage = 0;
 var kbdMode = false;
 
-var targets = []
+var pureTargets = []
 var sections = $('.section')
 for(var i = 0; i < sections.length; i++) {
   var section = $(sections[i])
-  if(section.attr('class').includes("sp_post")) { // post
-      console.log(section.find('li'));
-  }
-
-  console.log(section.attr('class'));
+  var sectionClass = section.attr('class')
+  // if(
+  //   sectionClass.includes('sp_post')
+  //   || sectionClass.includes('ndic')
+  //   || sectionClass.includes('nsite')
+  //   || sectionClass.includes('kinn')
+  //   || sectionClass.includes('news')
+  //   || sectionClass.includes('magazine')
+  //   || sectionClass.includes('book_list')
+  //   || sectionClass.includes('greenmap')
+  //   || sectionClass.includes('sp_mobile')
+  //   || sectionClass.includes('academic')
+  //   || sectionClass.includes('sp_video')
+  //   || sectionClass.includes('blog')
+  //   || sectionClass.includes('webdoc')
+  //   || sectionClass.includes('content_search')
+  //   || sectionClass.includes('cafe')
+  //   || sectionClass.includes('book_body')
+  //   || sectionClass.includes('result_more')
+  // ) { // post
+    var sectionLi = section.find('li')
+    for(var j = 0; j < sectionLi.length; j++) {
+      pureTargets.push(sectionLi[j])
+    }
+  // }
+  // console.log(section.attr('class'));
 }
+
+var targets = []
+for(var i = 0; i < pureTargets.length; i++) {
+  targets.push($(pureTargets[i]))
+}
+console.log(targets);
+
 document.body.addEventListener('keydown', function(e) {
-  if(e.key === '/'){
+  console.log(e.key);
+  if(e.key === 'Escape'){
     kbdMode = !kbdMode
     if(kbdMode) {
       resultPage = 0
@@ -30,46 +59,46 @@ document.body.addEventListener('keydown', function(e) {
     return;
   }
 
-  // if(e.key === 'ArrowDown') {
-  //   if(resultIndex != resultItems.length) {
-  //     resultIndex++;
-  //   }
-  //   activeAction()
-  //   e.preventDefault();
-  //   e.stopPropagation();
-  // } else if(e.key === 'ArrowUp') {
-  //   if(resultIndex !== 0) {
-  //     resultIndex--;
-  //   }
-  //   activeAction()
-  //   e.stopPropagation();
-  //   e.preventDefault();
-  // } else if(e.key === 'Enter') {
-  //   $($(resultItems[resultIndex]).find('a')[0])[0].click()
-  //   e.stopPropagation();
-  //   e.preventDefault();
-  // } else if(e.key === 'ArrowLeft') {
-  //   var preBtn = $('#pnprev')[0]
-  //   if(preBtn) preBtn.click()
-  // } else if(e.key === 'ArrowRight') {
-  //   var nextBtn = $('#pnnext')[0]
-  //   if(nextBtn) nextBtn.click()
-  // }
+  if(e.key === 'ArrowDown') {
+    if(resultIndex != targets.length) {
+      resultIndex++;
+    }
+    activeAction()
+    e.preventDefault();
+    e.stopPropagation();
+  } else if(e.key === 'ArrowUp') {
+    if(resultIndex !== 0) {
+      resultIndex--;
+    }
+    activeAction()
+    e.stopPropagation();
+    e.preventDefault();
+  } else if(e.key === 'Enter') {
+    $($(targets[resultIndex]).find('a')[0])[0].click()
+    e.stopPropagation();
+    e.preventDefault();
+  } else if(e.key === 'ArrowLeft') {
+    var preBtn = $('#pnprev')[0]
+    if(preBtn) preBtn.click()
+  } else if(e.key === 'ArrowRight') {
+    var nextBtn = $('#pnnext')[0]
+    if(nextBtn) nextBtn.click()
+  }
 });
 
 function clearResultBorder() {
-  var resultItems = $('.g')
-  for(var i=0; i < resultItems.length; i++) {
-    $(resultItems[i]).css('border', '0px');
+  for(var i=0; i < targets.length; i++) {
+    targets[i].css('border', '0px');
   }
 }
 
 function activeAction() {
-  var resultItems = $('.g')
-  if(resultIndex === resultItems.length) return;
+  console.log('activeAction');
+  console.log(targets);
+  if(resultIndex === targets.length) return;
   clearResultBorder()
-  var target = $(resultItems[resultIndex])
-  target.css({"border-color": "#4285f4",
+  var target = targets[resultIndex]
+  target.css({"border-color": "#00ab33",
              "border-width":"1.5px",
              "border-style":"solid"});
    $('html body').stop().animate({scrollTop:target.offset().top-100})
