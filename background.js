fd759d24116190f988bdb2ecef06606c5ad19369
gswon <bluemirr5@gmail.com>
2017-08-21 01:07:56 +0900

    refactoring & speed up

--- background.js
+++ background.js
@@ -1,9 +1,6 @@
 var excutedTabIds = [];
 chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
-  console.log(change.status);
   if (change.status == "complete") {
-    console.log(tabId)
-    console.log(excutedTabIds);
     find(tab, true);
   }
 });
@@ -15,32 +12,24 @@ chrome.tabs.onSelectionChanged.addListener(function(tabId, info) {
 });
 
 function find(tab, updated) {
-  console.log(tab.url);
   if(tab.url.includes("www.google")&&tab.url.includes("search?")) {
-    chrome.browserAction.setIcon({path:"google.png"});
+    chrome.browserAction.setIcon({path:"imgs/google.png"});
     chrome.browserAction.setTitle({title: "This page is Google Search Page"});
-    console.log('icon setting')
     if(excutedTabIds.indexOf(tab.id) == -1 || updated) {
       if(excutedTabIds.indexOf(tab.id) == -1) excutedTabIds.push(tab.id)
-      chrome.tabs.executeScript(tab.id, { file: "jquery-3.2.1.js"}, function(jresult) {
-        console.log(jresult);
-        chrome.tabs.executeScript(tab.id, { file:"content_script.js"}, function(result) {
-          console.log(result)
-        });
+      chrome.tabs.executeScript(tab.id, { file: "contentScript/content_script.js"}, function(jresult) {
       });
     }
   } else if(tab.url.includes("search.naver.com")&&tab.url.includes("search.naver?")) {
-    chrome.browserAction.setIcon({path:"naver.png"});
+    chrome.browserAction.setIcon({path:"imgs/naver.png"});
     chrome.browserAction.setTitle({title: "This page is Naver Search Page"});
     if(excutedTabIds.indexOf(tab.id) == -1 || updated) {
       if(excutedTabIds.indexOf(tab.id) == -1) excutedTabIds.push(tab.id)
-      chrome.tabs.executeScript(tab.id, { file: "jquery-3.2.1.js"}, function() {
-        chrome.tabs.executeScript(tab.id, { file:"naver_content_script.js"}, function() {
-        });
+      chrome.tabs.executeScript(tab.id, { file: "contentScript/naver_content_script.js"}, function() {
       });
     }
   } else {
-    chrome.browserAction.setIcon({path:"inactive.png"});
+    chrome.browserAction.setIcon({path:"imgs/inactive.png"});
     chrome.browserAction.setTitle({title: "This page is not Google Search Page"});
   }
 }
